7071bd6ee3a738c1ad1634dd323105f871501c38
jeanpiress <jean.m.pires@gmail.com>
2022-11-22 17:28:06 -0300

    Consulta simples com query methods

--- src/main/java/com/jeanpiress/workshopmongodb/services/PostService.java
+++ src/main/java/com/jeanpiress/workshopmongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.jeanpiress.workshopmongodb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,4 +22,9 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 			
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
+
+
+}
