91912bd4cedadc9fb0c841973436e664974095aa
jeanpiress <jean.m.pires@gmail.com>
2022-11-22 18:48:22 -0300

    Consulta simples com @Query

--- src/main/java/com/jeanpiress/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/jeanpiress/workshopmongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.jeanpiress.workshopmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.jeanpiress.workshopmongodb.domain.Post;
@@ -11,5 +12,8 @@ import com.jeanpiress.workshopmongodb.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 
 
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
